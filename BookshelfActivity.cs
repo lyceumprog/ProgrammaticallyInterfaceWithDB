f2dedcd4be55711bcc2bea0d02d8f60a1ac5c246
unknown-user <unknown-user@192.168.56.1>
2015-11-15 14:08:00 +0300

    Первая публикация дополнения ProgrammaticallyInterface

---
+++ BookshelfActivity.cs
@@ -0,0 +1,93 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Mono.Data.Sqlite;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace ProgrammaticallyInterface
+{
+	[Activity (Label = "BookshelfActivity")]			
+	public class BookshelfActivity : Activity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			new DatabaseCopy (this); // Копирование файла БД из папки Assets во внутреннюю память устройства
+
+			base.OnCreate (bundle);
+			SetContentView (Resource.Layout.BookshelfLayout);
+
+			string dbPath = System.IO.Path.Combine(System.Environment.GetFolderPath
+				(System.Environment.SpecialFolder.Personal),"library.sqlite"
+			); // Конечный путь к файлу БД на внутренней памяти телефона
+
+			SqliteConnection connection = new SqliteConnection ("Data source=" + dbPath); // Создание подключения к БД
+			SqliteCommand command = connection.CreateCommand (); // Создание запроса к БД
+			SqliteDataReader reader; // Объявление переменной чтения из входящего потока данных из БД
+
+			List<string> book_name = new List<string> (); // Объявление переменной со списком книг
+
+			connection.Open (); // Открытие подключения к БД
+			command.CommandText = "SELECT name FROM book"; // Создание текста запроса к БД
+			reader = command.ExecuteReader (); // Получение потока данных из БД
+			while (reader.Read ()) { // Пока есть считываемые массивы данных
+				book_name.Add (reader [0].ToString ()); // Сохраняем в списке книг их названия
+			}
+			reader.Close (); // Закрываем поток чтения из БД
+			connection.Close (); // Закрываем подключение к БД
+
+			LinearLayout mainLayout = 
+				FindViewById<LinearLayout> 
+				(Resource.Id.linearLayout2); // Получение доступа к основному контейнеру, куда будут вставляться остальные контейнеры с книгами
+			
+			for (int i = 0; i < book_name.Count / 2 + book_name.Count % 2; i++) { // Цикл по рядам книг
+				LinearLayout newLinearLayout = 
+					new LinearLayout (this); // Создание горизонтального контейнера, куда будут вставляться книги
+				newLinearLayout.Orientation = 
+					Orientation.Horizontal;
+
+				for (int j = 0; j < 2; j ++) { // Цикл по столбцам ряда
+					if (i * 2 + j < book_name.Count) { // Проверка неполного ряда
+						Button newButton = new Button (this); // Создание кнопки(книги)
+						newButton.Text = book_name [i * 2 + j]; // Присваивание ей текста
+
+
+						if ((i * 2 + j+1)%3 == 0)
+							newButton.SetBackgroundResource (Resource.Drawable.book1); // Объявление заднего фона для кнопки
+						else if ((i*2 + j +1)%2 ==0)
+							newButton.SetBackgroundResource (Resource.Drawable.book2); // Объявление заднего фона для кнопки
+						else
+							newButton.SetBackgroundResource (Resource.Drawable.book3); // Объявление заднего фона для кнопки
+
+						LinearLayout.LayoutParams buttonParameters = 
+							new LinearLayout.LayoutParams (100, 100); // Задание параметров положения для кнопки
+						buttonParameters.SetMargins
+						(0, 0, 5, 5); // Задание отступов
+
+						newButton.LayoutParameters = buttonParameters; 
+
+						newButton.Click += (object sender, EventArgs e) => 
+						{
+							// Обработка нажатия на кнопку
+						};
+
+						newLinearLayout.AddView (newButton); // Добавление на созданный контейнер созданной кнопки
+					}
+				}
+
+				mainLayout.AddView (newLinearLayout); // Добавление на основной контейнер созданный контейнер
+			}
+
+			// Create your application here
+		}
+	}
+}
+
---
+++ MainActivity.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace ProgrammaticallyInterface
+{
+	[Activity (Label = "ProgrammaticallyInterface", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity
+	{
+		int count = 1;
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+
+			// Get our button from the layout resource,
+			// and attach an event to it
+
+			RelativeLayout mainLayout = 
+				FindViewById<RelativeLayout> 
+				(Resource.Id.relativeLayout1);
+
+			Button b1 = new Button (this);
+			b1.Text = "1";
+			b1.Id = 1;
+
+			b1.Click += delegate {
+				StartActivity(typeof(BookshelfActivity));
+			};
+
+			Button b2 = new Button (this);
+			b2.Text = "2";
+			b2.Id = 2;
+
+			RelativeLayout.LayoutParams b2Settings = 
+				new RelativeLayout.LayoutParams 
+				(ViewGroup.LayoutParams.WrapContent, 40);
+			b2Settings.AddRule (LayoutRules.Below, b1.Id);
+			b2Settings.AddRule (LayoutRules.RightOf, b1.Id);
+			b2.LayoutParameters = b2Settings;
+
+			Button b3 = new Button (this);
+			b3.Text = "3";
+			b3.Id = 3;
+
+			RelativeLayout.LayoutParams b3Settings = 
+				new RelativeLayout.LayoutParams 
+				(ViewGroup.LayoutParams.WrapContent, 40);
+			b3Settings.AddRule (LayoutRules.RightOf, b2.Id);
+			b3.LayoutParameters = b3Settings;
+
+			Button b4 = new Button (this);
+			b4.Text = "4";
+			b4.Id = 4;
+
+			RelativeLayout.LayoutParams b4Settings = new RelativeLayout.LayoutParams (ViewGroup.LayoutParams.WrapContent, 40);
+			b4Settings.AddRule (LayoutRules.RightOf, b3.Id);
+			b4Settings.AddRule (LayoutRules.Below, b3.Id);
+			b4.LayoutParameters = b4Settings;
+
+			mainLayout.AddView (b1);
+			mainLayout.AddView (b2);
+			mainLayout.AddView (b3);
+			mainLayout.AddView (b4);
+		}
+	}
+}
+
+
